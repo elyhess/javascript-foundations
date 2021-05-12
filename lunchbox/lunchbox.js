c3c9b0bdbc178707aaa9b250ac731047642a6cfe
Ely Hess <ely.hess@me.com>
2021-05-12 15:52:20 -0600

    Add completed code

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,26 @@
 class LunchBox {
+  constructor(data) {
+    this.owner = data.owner
+    this.material = data.madeOf
+    this.shape = data.shape
+    this.color = data.color
+    this.snacks = []
+  }
 
+  acquireSnack(snack) {
+    snack.isInLunchBox = true
+    this.snacks.push(snack)
+  }
+
+  findHealthySnacks() {
+    const container = [];
+    this.snacks.forEach(item => {
+      if (item.checkForHealthy()) {
+        container.push(item.type)
+      }
+    });
+    return container
+  }
 }
 
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,4 +1,22 @@
 class Snack {
+  constructor(type) {
+    this.type = type
+    this.deliciousLevel = "extra"
+    this.amount = 100
+    this.isInLunchBox = false
+    this.cuttingItClose = false
+  }
+
+  getEaten() {
+    this.amount -= 10
+    if (this.amount <= 20) {
+      this.cuttingItClose = true
+    }
+  }
+
+  checkForHealthy() {
+    return !!(this.type.includes("fruit") || this.type.includes("Fruit"));
+  }
 
 }
 
